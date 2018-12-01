46a060fc4e108d438984f98a55c16af54fd7c847
azlen <azlen.elz@gmail.com>
2018-12-01 13:49:51 -0800

    add icon to reactivate bottom bar

--- lib/bottom_app_bar.dart
+++ lib/bottom_app_bar.dart
@@ -1,31 +1,5 @@
 import 'package:flutter/material.dart';
 
-class BottomBarHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bottom App Bar'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          OutlineButton(
-            child: const Text('Bottom App Bar'),
-            onPressed: () => Navigator.pushNamed(context, '/bottom_app_bar'),
-          ),
-          OutlineButton(
-            child: const Text('Google Tasks Bottom bar'),
-            onPressed: () =>
-                Navigator.pushNamed(context, '/bottom_app_bar_google'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class BottomAppBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -43,10 +17,10 @@ class BottomAppBarPage extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            //IconButton(
-            //  icon: Icon(Icons.menu),
-            //  onPressed: () {},
-            //),
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
           ],
         ),
       ),
